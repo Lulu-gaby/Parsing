70c9e174d59d0bf5cf6126ef7b757cef2286146f
Lulu-gaby <lulugabyxxx@gmail.com>
2025-04-14 19:37:21 +0300

    new code added

---
+++ Web-scraping/divanlightspars/divanlightspars/spiders/divanlights.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class DivanlightsSpider(scrapy.Spider):
+    name = "divanlights"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lights = response.css('div._Ud0k')
+        for light in lights:
+            yield {
+                'name': light.css('div.lsooF span::text').get(),
+                'price': light.css('div.pY3d2 span::text').get(),
+                'url': light.css('a').attrib['href']
+
+            }
